4fc347a42741b85649efb503f48a215400a58cb6
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-05-15 18:27:25 +0200

    Update st_function.c

--- st_function.c
+++ st_function.c
@@ -34,6 +34,64 @@ int _strlen(const char *s)
 	return ((i + 1));
 }
 
+
+/**
+ * _strdup - copy a string to another
+ * @str: the array
+ * Return: pointer of sting copied
+ */
+char *_strdup(char *str)
+{
+	int k, l;
+	char *s = NULL;
+
+	if (str == NULL)
+		return (0);
+	for (k = 0; str[k]; k++)
+		;
+	k++;
+	s = malloc(k * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (l = 0; l < k; l++)
+	{
+		s[l] = str[l];
+	}
+	return (s);
+}
+
+/**
+ * _itoa - integer to ascii
+ * @num: num
+ * @b: base
+ * Return: char
+ **/
+char *_itoa(int num, int b)
+{
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do      {
+		*--ptr = array[n % b];
+		n /= b;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
 /**
  * _putchar - writes c to stdout
  * @c: character to print
@@ -46,7 +104,7 @@ int _putchar(char c)
 }
 
 /**
- *str_concat - back a pointer to array
+ *str_concat - point back a pointer to array
  *@s1: Array one
  *@s2: Array two
  *Return: Always an array dynamic
