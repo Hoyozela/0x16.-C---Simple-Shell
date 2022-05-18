96ff768390d7144a0f42c0bfa3260e2a9ae1b754
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-05-18 19:08:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,35 @@
 #include "shell.h"
 
 /**
- * main - creates a prompt to read input, sparses it, executes and awaits
- * another command unless exit command is used
- * @arc: number of arguemnets
- * @arrv: array of arguements
+ * main - creates a prompt that reads input, sparses it, executes and waits
+ * for another command unless told to exit
+ * @ac: numb of arguemnets
+ * @av: array of arguements
  * @env: environment variable
  * Return: EXIT_SUCCESS
  */
-int main(int ac __attribute__((unused)), char **arrv, char **env)
+int main(int ac __attribute__((unused)), char **av, char **env)
 {
 	char *line;
 	char **args, **path;
 	int count = 0, status = 0;
-	(void) arrv;
+	(void) av;
 	signal(SIGINT, handle_signal);
 	while (1)
 	{
 		prompt();
 		/*read input and return string*/
 		line = read_input();
-		/*separates string to get command */
+		/*separates string to get command and args*/
 		args = sparse_str(line, env);
 
 		if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
 		{
 			count += 1;
-			path = search_path(env); /*look for PATH in environment variable*/
+			path = search_path(env); /*Path search*/
 			status = _stat(args, path);
-			child_process(arrv, args, env, status, count);
+			child_process(av, args, env, status, count);
 		}
-		
 		else
 		{
 			free(args);
